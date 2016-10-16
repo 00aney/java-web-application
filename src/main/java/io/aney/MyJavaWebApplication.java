ed2f0c01d5261bfc4831a0588c35fd1f81e44ce7
김태동 <yenafirst91@gmail.com>
2016-10-16 14:50:22 +0900

    initial commit

---
+++ src/main/java/io/aney/MyJavaWebApplication.java
@@ -0,0 +1,12 @@
+package io.aney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJavaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJavaWebApplication.class, args);
+	}
+}
---
+++ src/test/java/io/aney/MyJavaWebApplicationTests.java
@@ -0,0 +1,16 @@
+package io.aney;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyJavaWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
