f77118f862a292241b8dc65ad7ea4545347b8df9
김태동 <yenafirst91@gmail.com>
2016-10-19 01:25:40 +0900

    added JPA, H2 Database

--- src/main/java/io/aney/domain/User.java
+++ src/main/java/io/aney/domain/User.java
@@ -1,6 +1,17 @@
-package io.aney.web;
+package io.aney.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Column(nullable=false, length=20)
 	private String userId;
 	private String name;
 	private String password;
