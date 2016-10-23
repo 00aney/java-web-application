cfc2c8230c2414a2c83157d73b45ff3c187e8a53
김태동 <yenafirst91@gmail.com>
2016-10-24 04:49:04 +0900

    HTML 정리, URL  리팩토링

--- src/main/java/io/aney/web/UserController.java
+++ src/main/java/io/aney/web/UserController.java
@@ -8,28 +8,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import io.aney.domain.User;
 import io.aney.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	private List<User> users = new ArrayList<User>();
 	
 	@Autowired
 	private UserRepository userRepository;
 	
-	@PostMapping("/create")
-	public String create(User user) {
+	@PostMapping("")
+	public String createUser(User user) {
 		System.out.println("user : " + user);
 		users.add(user);
 		userRepository.save(user);
-		return "redirect:/list";
+		return "redirect:/users";
 	}
 	
-	@GetMapping("/list")
-	public String list(Model model) {
+	@GetMapping("")
+	public String listUser(Model model) {
 		model.addAttribute("users",	userRepository.findAll());
-		return "list";
+		return "user/list";
 	}
 }
