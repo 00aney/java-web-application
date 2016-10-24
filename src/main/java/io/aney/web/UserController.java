bd2da85f5f53589a782acd097b939b65f0d2835b
김태동 <yenafirst91@gmail.com>
2016-10-25 02:47:53 +0900

    개인정보 수정 기능 구현, put

--- src/main/java/io/aney/web/UserController.java
+++ src/main/java/io/aney/web/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import io.aney.domain.User;
@@ -45,5 +46,13 @@ public class UserController {
 	public String updateForm(@PathVariable Long id, Model model) {
 		model.addAttribute("user", userRepository.findOne(id));
 		return "/user/updateForm";
+	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable Long id, User newUser) {
+		User user = userRepository.findOne(id);
+		user.update(newUser);
+		userRepository.save(user);
+		return "redirect:/users";
 	}
 }
